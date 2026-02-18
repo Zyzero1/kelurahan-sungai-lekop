b560a8fb1b429cf55bc997b95f0ad7c7306dcd9a
Zyzero1 <mazza050901@gmail.com>
2026-02-18 18:19:18 +0700

    add favicon website

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -4,7 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <title>Kelurahan Sungai Lekop</title>
-    <link rel="icon" type="image/png" href="{{ asset('images/Bintan-Logo.png') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon-16x16.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('images/android-chrome-192x192.png') }}">
+    <link rel="icon" type="image/png" sizes="512x512" href="{{ asset('images/android-chrome-512x512.png') }}">
     @vite('resources/css/app.css')
 </head>
 
